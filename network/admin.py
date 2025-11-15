bb8f8eb34e8c75e71332189a4ea7b63e2b262281
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-11-15 21:46:30 +0300

    enhance admin panel with supplier links, city filters, debt clearance action, and search

--- network/admin.py
+++ network/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Contact, Product, NetworkNode
 
 @admin.register(Contact)
@@ -11,9 +13,27 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'supplier', 'get_level', 'debt', 'created_at']
+    list_display = ['name', 'supplier_link', 'get_level', 'debt', 'created_at']
+    list_filter = ['contact__city']
+    actions = ['clear_debt']
+    search_fields = ['name']
+
+    def supplier_link(self, obj):
+        if obj.supplier:
+            return format_html('<a href="{}">{}</a>',
+                               f'../networknode/{obj.supplier.id}/',
+                               obj.supplier.name)
+        return "-"
+
+    supplier_link.short_description = 'Supplier'
 
     def get_level(self, obj):
         return obj.level
 
     get_level.short_description = 'Level'
+
+    def clear_debt(self, request, queryset):
+        updated = queryset.update(debt=0)
+        self.message_user(request, f"Задолженность очищена у {updated} объектов")
+
+    clear_debt.short_description = "Очистить задолженность у выбранных объектов"
