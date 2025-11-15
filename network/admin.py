66008ba8c2689505faa9ae7f0de17aab0d896b72
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-11-15 21:30:51 +0300

    complete models hierarchy with recursive level calculation

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Contact, Product, NetworkNode
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['email', 'country', 'city']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'model', 'release_date']
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'supplier', 'get_level', 'debt', 'created_at']
+
+    def get_level(self, obj):
+        return obj.level
+
+    get_level.short_description = 'Level'
